0c6146e73d3c66e0631876ebacbe3b5cdc20971a
WiktorProsowicz <prosowicz.wiktor@gmail.com>
2024-11-06 14:07:27 +0000

    Add Docker environment setup

---
+++ .devcontainer/res/post_create_command.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This script should be run after the devcontainer is created.
+# It's role is to establish the necessary environment for the
+# user to start developing.
+
+deactivate 2> /dev/null
+
+rm -rf venv
+
+python3.12 project_setup.py setup_venv && source venv/bin/activate
+
+pip install -r requirements.txt
+pip install -r requirements-dev.txt
+
+# Log colorfully
+cat .devcontainer/res/.bash_aliases >> ~/.bash_aliases
+cat .devcontainer/res/.bashrc >> ~/.bashrc

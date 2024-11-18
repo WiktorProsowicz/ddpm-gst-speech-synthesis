d8d08144428c9d5a3b657bc9ad57d312d7fd49ea
WiktorProsowicz <prosowicz.wiktor@gmail.com>
2024-11-18 17:34:38 +0000

    Implement train_model script

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 """Contains training pipeline for the model."""
+
+from typing import Any, Dict, Tuple
 import argparse
 import logging
 import os
@@ -16,6 +18,9 @@ from torch.utils import tensorboard as torch_tb
 from data import data_loading
 from utilities import logging_utils
 from utilities import scripts_utils
+from models.ddpm_gst_speech_gen import utils as m_utils
+from models.ddpm_gst_speech_gen import training
+from utilities import diffusion as diff_utils
 
 HOME_PATH = pathlib.Path(__file__).absolute().parent.parent.parent.as_posix()
 SCRIPT_PATH = os.path.join(HOME_PATH, 'scripts', 'train_model')
@@ -28,20 +33,78 @@ DEFAULT_CONFIG = {
     },
     'training': {
         'batch_size': 64,
-        'epochs': 32,
+        "lr": 2e-4,
+        'validation_interval': 100,
+        'steps': 1000,
+        'start_step': 0,
+        'checkpoint_interval': 200,
+        'checkpoints_path': scripts_utils.CfgRequired(),
+
+        'diffusion': {
+            'n_steps': 400,
+            'beta_min': 0.0001,
+            'beta_max': 0.02,
+        }
     },
     'model': {
-        'encoder_input_shape': [256, 80],
-        'decoder_input_shape': [1024, 80],
+        'decoder': {
+            'timestep_embedding_dim': 128,
+        },
+        'gst': {
+            'use_gst': True,
+            'embedding_dim': 256,
+            'token_count': 32,
+        }
     }
 }
+
+
+def _get_model_trainer(
+        input_spectrogram_shape,
+        input_phonemes_shape,
+        train_loader: torch_data.DataLoader,
+        val_loader: torch_data.DataLoader,
+        config: Dict[str, Any],
+        tb_writer: torch_tb.SummaryWriter
+) -> training.ModelTrainer:
+
+    checkpoints_handler = m_utils.ModelCheckpointHandler(
+        config['training']['checkpoints_path'], 'ddpm_gst_speech_gen_ckpt')
+
+    model_components = m_utils.create_model_components(
+        input_spectrogram_shape, input_phonemes_shape, config['model'])
+
+    def model_provider():
+
+        if checkpoints_handler.num_checkpoints() > 0:
+            checkpoint, _ = checkpoints_handler.get_newest_checkpoint(model_components)
+            return checkpoint
+
+        return model_components
+
+    return training.ModelTrainer(
+        model_provider,
+        train_loader,
+        val_loader,
+        tb_writer,
+        torch.device('cuda' if torch.cuda.is_available() else 'cpu'),
+        diff_utils.LinearScheduler(
+            config['training']['diffusion']['beta_min'],
+            config['training']['diffusion']['beta_max'],
+            config['training']['diffusion']['n_steps']
+        ),
+        checkpoints_handler,
+        config['training']['checkpoint_interval'],
+        config['training']['validation_interval'],
+        config['training']['lr']
+    )
 
 
 def _log_example_data(train_ds: torch_data.Dataset, tb_writer: torch_tb.SummaryWriter):
 
     example_data = train_ds[np.random.randint(0, len(train_ds))]
 
-    spec, transcript, durations = example_data
+    spec, _, _ = example_data
 
     tb_writer.add_image('example_mel_spectrograms', np.expand_dims(spec, axis=0))
 
@@ -80,6 +143,25 @@ def main(config):
 
     logging.info('Data loaders created.')
 
+    input_spectrogram_shape = train_ds[0][0].shape
+    input_phonemes_shape = train_ds[0][1].shape
+
+    model_trainer = _get_model_trainer(
+        input_spectrogram_shape,
+        input_phonemes_shape,
+        train_loader,
+        val_loader,
+        config,
+        tb_writer)
+
+    logging.info('Running training for %d steps starting from %d...',
+                 config['training']['steps'],
+                 config['training']['start_step'])
+
+    model_trainer.run_training(config['training']['steps'],
+                               config['training']['start_step'],
+                               use_profiler=True)
+
     tb_writer.close()
 
 
@@ -103,5 +185,5 @@ if __name__ == '__main__':
 
     args = _get_cl_args()
 
-    config = scripts_utils.try_load_user_config(args.config_path, DEFAULT_CONFIG)
-    main(config)
+    configuration = scripts_utils.try_load_user_config(args.config_path, DEFAULT_CONFIG)
+    main(configuration)

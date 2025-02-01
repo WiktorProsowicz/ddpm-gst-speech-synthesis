d76dd71ad3d75e2b53b54e6eddffe0b1cab22dbf
WiktorProsowicz <prosowicz.wiktor@gmail.com>
2025-02-01 09:48:58 +0100

    Update gst dataset script
    Disabled gradient calculation
    Added getting ds statistics

--- scripts/dataset/prepare_gst_ds.py
+++ scripts/dataset/prepare_gst_ds.py
@@ -41,6 +41,7 @@ def main(config):
     logging.info('Serializing the GST Predictor\'s dataset.')
 
     sample_names = filter(lambda path: '.pt' in path, os.listdir(config['processed_ds_path']))
+    sample_names = list(sample_names)
 
     for sample_idx, sample_name in enumerate(sample_names):
         data_sample_path = os.path.join(config['processed_ds_path'], sample_name)
@@ -49,14 +50,44 @@ def main(config):
         spectrogram = torch.unsqueeze(spectrogram, dim=0)
         phonemes = torch.unsqueeze(phonemes, dim=0)
 
-        enhanced_phonemes = acoustic_model_comps.encoder.run_basic_blocks(phonemes)
-        gst_embedding = acoustic_model_comps.embedder(spectrogram, acoustic_model_comps.gst())
+        with torch.no_grad():
+            enhanced_phonemes = acoustic_model_comps.encoder.run_basic_blocks(phonemes)
+            gst_embedding = acoustic_model_comps.embedder(spectrogram, acoustic_model_comps.gst())
+
+        enhanced_phonemes = enhanced_phonemes.squeeze(dim=0)
+        gst_embedding = gst_embedding.squeeze(dim=0)
 
         output_path = os.path.join(config['output_path'], sample_name)
         torch.save((enhanced_phonemes, gst_embedding), output_path)
 
         if (sample_idx + 1) % 1000 == 0:
             logging.debug('Processed %d samples.', sample_idx + 1)
+
+    logging.info('Calculating the dataset statistics.')
+
+    gst_embedding_mean = torch.zeros((config['acoustic_model_cfg']['d_model']))
+    gst_embedding_std = torch.zeros((config['acoustic_model_cfg']['d_model']))
+
+    for sample_name in sample_names:
+        sample_path = os.path.join(config['output_path'], sample_name)
+        _, embedding = torch.load(sample_path, weights_only=True)
+
+        gst_embedding_mean += embedding
+
+    gst_embedding_mean /= len(sample_names)
+
+    for sample_name in sample_names:
+        sample_path = os.path.join(config['output_path'], sample_name)
+        _, embedding = torch.load(sample_path, weights_only=True)
+
+        gst_embedding_std += (embedding - gst_embedding_mean) ** 2
+
+    gst_embedding_std /= len(sample_names)
+
+    os.makedirs(os.path.join(config['output_path'], 'stats'), exist_ok=True)
+    stats_path = os.path.join(config['output_path'], 'stats', 'gst_embedding_stats.pt')
+
+    torch.save((gst_embedding_mean, gst_embedding_std), stats_path)
 
 
 if __name__ == '__main__':

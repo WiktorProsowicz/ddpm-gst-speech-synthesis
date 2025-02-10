c6ebd98d58a8cc703bfd04d03f838484f5b43551
WiktorProsowicz <prosowicz.wiktor@gmail.com>
2025-02-10 19:44:43 +0000

    Rebuild inference script
    Made the script use simpler compiled model API
    Incorporated HiFiGAN Vocoder

--- scripts/inference/run_inference.py
+++ scripts/inference/run_inference.py
@@ -6,58 +6,109 @@ and mel2linear converter.
 
 For script's configuration, see `DEFAULT_CONFIG` constant.
 """
-import argparse
+import os
 import logging
+import json
 
 import torch
 import torchaudio
 from torchvision import transforms
+from torchaudio.prototype.pipelines import HIFIGAN_VOCODER_V3_LJSPEECH as hifigan_bundle
 
 from data.preprocessing import text as text_prep
 from utilities import logging_utils
 from utilities import scripts_utils
+from utilities import diffusion as diff_utils
+from utilities import inference
 
 DEFAULT_CONFIG = {
     'compiled_model_path': scripts_utils.CfgRequired(),
-    'input_phonemes_length': 20,
     'input_text': scripts_utils.CfgRequired(),
-    # Can be one of ('none', 'reference', 'weights')
-    'gst_mode': 'none',
-    'scale_min': -100.0,
-    'scale_max': 41.0,
-    'output_path': scripts_utils.CfgRequired(),
-    'gst_reference_cfg': {
-        # Path to the audio file to use as reference for GST embedding
-        'reference_audio_path': None,
-        'spectrogram_window_length': 1024,
-        'spectrogram_hop_length': 256,
-        'n_mels': 80,
-        'spec_length': 200,
-        'sample_rate': 22050,
-    },
-    'gst_weights_cfg': {
-        'weights_path': None
-    }
+    'output_path': scripts_utils.CfgRequired()
 }
 
 
-def _scale_spec(spectrogram: torch.Tensor, target_min: float, target_max: float) -> torch.Tensor:
-    """Scales the spectrogram to the target range."""
+def _obtain_style_embedding(phoneme_representations: torch.Tensor,
+                            gst_predictor_encoder: torch.jit.ScriptModule,
+                            gst_predictor_decoder: torch.jit.ScriptModule,
+                            config,
+                            device: torch.device) -> torch.Tensor:
 
-    min_val = spectrogram.min()
-    max_val = spectrogram.max()
+    param_scheduler = diff_utils.LinearScheduler(config['diff_beta_min'],
+                                                 config['diff_beta_max'],
+                                                 config['diff_timesteps'])
 
-    return (spectrogram - min_val) / (max_val - min_val) * (target_max - target_min) + target_min
+    diffusion_handler = diff_utils.DiffusionHandler(param_scheduler, device)
+
+    with torch.no_grad():
+
+        phoneme_embedding = gst_predictor_encoder(phoneme_representations)
+
+        noised_style_embedding = torch.randn(1, config['style_embedding_size'], device=device)
+
+        for diff_timestep in reversed(range(config['diff_timesteps'])):
+
+            predicted_noise = gst_predictor_decoder(
+                noised_style_embedding,
+                torch.tensor([diff_timestep], device=device),
+                phoneme_embedding
+            )
+
+            noised_style_embedding = diffusion_handler.remove_noise(noised_style_embedding,
+                                                                    predicted_noise,
+                                                                    diff_timestep)
+
+    return noised_style_embedding
 
 
 def main(config):  # pylint: disable=too-many-locals
     """Loads the model and runs inference."""
 
-    # if config['gst_weights'] is None and config['reference_audio_path'] is None:
-    #     raise ValueError('Either `gst_weights` or `reference_audio_path` must be provided.')
+    metadata_path = os.path.join(config['compiled_model_path'], 'metadata.json')
+    with open(metadata_path, 'r', encoding='utf-8') as metadata_file:
+        metadata = json.load(metadata_file)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     logging.info('Loading the compiled model...')
-    compiled_model = torch.jit.load(config['compiled_model_path'])
+
+    acoustic_encoder_path = os.path.join(config['compiled_model_path'], 'acoustic_encoder.pt')
+    acoustic_encoder = torch.jit.load(acoustic_encoder_path).to(device)
+
+    acoustic_decoder_path = os.path.join(config['compiled_model_path'], 'acoustic_decoder.pt')
+    acoustic_decoder = torch.jit.load(acoustic_decoder_path).to(device)
+
+    acoustic_encoder.eval()
+    acoustic_decoder.eval()
+
+    if metadata['gst_predictor_cfg'] is not None:
+
+        logging.info('Loading the GST predictor components...')
+
+        encoder_path = os.path.join(config['compiled_model_path'], 'gst_predictor_encoder.pt')
+        decoder_path = os.path.join(config['compiled_model_path'], 'gst_predictor_decoder.pt')
+
+        gst_predictor_encoder = torch.jit.load(encoder_path).to(device)
+        gst_predictor_decoder = torch.jit.load(decoder_path).to(device)
+
+        gst_predictor_encoder.eval()
+        gst_predictor_decoder.eval()
+
+        def get_style_emb(phoneme_representations):
+            return _obtain_style_embedding(
+                phoneme_representations,
+                gst_predictor_encoder,
+                gst_predictor_decoder,
+                metadata['gst_predictor_cfg'],
+                device
+            )
+
+    else:
+        def get_style_emb(_):
+            return None
+
+    logging.info('Logging HiFi-GAN vocoder...')
+    vocoder = hifigan_bundle.get_vocoder().to(device)
 
     logging.info('Transforming the input text to phonemes...')
     all_input_phonemes = text_prep.G2PTransform()(config['input_text'])
@@ -65,92 +116,52 @@ def main(config):  # pylint: disable=too-many-locals
     logging.debug('Input phonemes: %s', all_input_phonemes)
 
     phonemes_transform = transforms.Compose([
-        text_prep.PadSequenceTransform(config['input_phonemes_length']),
+        text_prep.PadSequenceTransform(metadata['input_phonemes_length']),
         text_prep.OneHotEncodeTransform(text_prep.ENHANCED_MFA_ARP_VOCAB)
-    ])
-
-    output_transform = transforms.Compose([
-        transforms.Lambda(lambda x: torchaudio.functional.DB_to_amplitude(x, ref=1.0, power=0.5)),
-        torchaudio.transforms.GriffinLim(n_fft=1024,
-                                         win_length=1024,
-                                         hop_length=256,
-                                         power=1),
     ])
 
     logging.info('Running inference...')
 
-    total_output_lin_spec = None
+    total_output_waveform = None
 
-    for run_idx in range((len(all_input_phonemes) // config['input_phonemes_length']) + 1):
+    for run_idx in range((len(all_input_phonemes) // metadata['input_phonemes_length']) + 1):
         input_phonemes = all_input_phonemes[
-            run_idx * config['input_phonemes_length']:
-            (run_idx + 1) * config['input_phonemes_length']
+            run_idx * metadata['input_phonemes_length']:
+            (run_idx + 1) * metadata['input_phonemes_length']
         ]
 
-        input_phonemes = phonemes_transform(input_phonemes).unsqueeze(0)
-
-        if config['gst_mode'] == 'reference':
-
-            cfg = config['gst_reference_cfg']
-
-            ref_audio, original_sr = torchaudio.load(cfg['reference_audio_path'])
-
-            ref_speech_transform = transforms.Compose([
-                torchaudio.transforms.Resample(original_sr, cfg['sample_rate']),
-                torchaudio.transforms.MelSpectrogram(sample_rate=cfg['sample_rate'],
-                                                     n_fft=cfg['spectrogram_window_length'],
-                                                     win_length=cfg['spectrogram_window_length'],
-                                                     hop_length=cfg['spectrogram_hop_length'],
-                                                     n_mels=cfg['n_mels']),
-                torchaudio.transforms.AmplitudeToDB(),
-                transforms.Lambda(lambda x: _scale_spec(x, 0.0, 1.0))
-            ])
-
-            ref_speech = ref_speech_transform(ref_audio)
-
-            if (run_idx + 1) * cfg['spec_length'] >= ref_speech.shape[2]:
-                ref_speech = ref_speech[:, :,
-                                        run_idx * cfg['spec_length']:
-                                        (run_idx + 1) * cfg['spec_length']]
-
-            else:
-                ref_speech = ref_speech[:, :, :cfg['spec_length']]
-
-            model_input = (input_phonemes, ref_speech)
-
-        elif config['gst_mode'] == 'weights':
-
-            cfg = config['gst_weights_cfg']
-            gst_weights = torch.load(cfg['weights_path'], weights_only=True)
-
-            model_input = (input_phonemes, gst_weights.unsqueeze(0))
-
-        else:
-            model_input = (input_phonemes,)
+        input_phonemes = phonemes_transform(input_phonemes).unsqueeze(0).to(device)
 
         with torch.no_grad():
-            output_lin_spec, log_durations = compiled_model(model_input)
+            transcript_mask = inference.create_transcript_mask(input_phonemes)
+            phoneme_representations = acoustic_encoder(input_phonemes)
+
+            style_embedding = get_style_emb(phoneme_representations)
+
+            if style_embedding is not None:
+                output_spec, log_durations = acoustic_decoder((phoneme_representations,
+                                                              transcript_mask,
+                                                              style_embedding))
+
+            else:
+                output_spec, log_durations = acoustic_decoder((phoneme_representations,
+                                                              transcript_mask))
 
             durations_mask = (log_durations > 0).to(torch.int64)
             durations = (torch.pow(2.0, log_durations) + 1e-4).to(torch.int64) * durations_mask
             total_dur = durations.sum()
-            output_lin_spec = output_lin_spec[:, :, :total_dur]
+            output_spec = output_spec[:, :, :total_dur]
 
-            if total_output_lin_spec is None:
-                total_output_lin_spec = output_lin_spec
+            waveform = vocoder(output_spec)
+
+            if total_output_waveform is None:
+                total_output_waveform = waveform[0]
 
             else:
-                total_output_lin_spec = torch.cat([total_output_lin_spec, output_lin_spec], dim=2)
-
-    total_output_lin_spec = _scale_spec(
-        total_output_lin_spec,
-        config['scale_min'],
-        config['scale_max'])
-
-    waveform = output_transform(total_output_lin_spec)
+                total_output_waveform = torch.cat([total_output_waveform, waveform[0]], dim=1)
 
     logging.info("Saving the output waveform to '%s'", config['output_path'])
-    torchaudio.save(config['output_path'], waveform, 22050)
+    torchaudio.save(config['output_path'], total_output_waveform.cpu(), 22050)
 
 
 if __name__ == '__main__':

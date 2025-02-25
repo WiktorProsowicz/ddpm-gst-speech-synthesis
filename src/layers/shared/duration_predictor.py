a6173af1992d15de15ba6071dc0e96b39d896463
WiktorProsowicz <prosowicz.wiktor@gmail.com>
2025-02-25 07:56:33 +0000

    Add ReLU to duration predictor final layer

--- src/layers/shared/duration_predictor.py
+++ src/layers/shared/duration_predictor.py
@@ -57,6 +57,7 @@ class DurationPredictor(torch.nn.Module):
                                                 dropout_rate=dropout_rate)
 
         self._output_layer = torch.nn.Linear(input_features, 1)
+        self._output_act = torch.nn.ReLU()
 
     def forward(self, encoder_output: torch.Tensor) -> torch.Tensor:
         """Predicts the durations of the input phonemes.
@@ -71,4 +72,4 @@ class DurationPredictor(torch.nn.Module):
         output = encoder_output.transpose(1, 2)
         output = self._conv_blocks(output)
         output = output.transpose(1, 2)
-        return self._output_layer(output)
+        return self._output_act(self._output_layer(output))

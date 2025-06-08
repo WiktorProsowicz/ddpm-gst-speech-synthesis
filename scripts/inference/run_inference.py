45b220dd42a2ff7222b990b195fe35d43333c615
WiktorProsowicz <prosowicz.wiktor@gmail.com>
2025-06-08 18:09:41 +0000

    Update run_inference script

--- scripts/inference/run_inference.py
+++ scripts/inference/run_inference.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 """Loads trained model components and runs inference on a given input.
 
-The script preprocesses and encodes the input text, loads the acoustic model
-and mel2linear converter.
-
 For script's configuration, see `DEFAULT_CONFIG` constant.
 """
 import json
@@ -36,6 +33,8 @@ DEFAULT_CONFIG = {
     'gst_pred_ckpt': scripts_utils.CfgOptional(None),
     # Path to a chosen preprocessed sample as expected by the GST Predictor trainer.
     'gst_pred_input_sample': scripts_utils.CfgOptional(None),
+    # Tells how much from the deterministically calculated GST embedding to use in the final one
+    'deterministic_gst_weight': scripts_utils.CfgOptional(None),
 
     'output_path': scripts_utils.CfgRequired()
 }
@@ -48,7 +47,7 @@ def _load_acoustic_model_components(acoustic_config,
                                     device: torch.device):
 
     ckpt_handler = tdu.serialization.ModelCheckpointHandler(
-        acoustic_config['checkpoints_path'],
+        acoustic_config['training']['checkpoints_path'],
         device,
         missing_modules_strict=False
     )
@@ -68,17 +67,19 @@ def _load_acoustic_model_components(acoustic_config,
 def _load_gst_pred_components(gst_pred_config,
                               ckpt: str,
                               input_phonemes_shape: Tuple[int, int],
-                              input_gst_size: int,
+                              gst_embedding_size: int,
+                              gst_weights_size: int,
                               device: torch.device):
 
     ckpt_handler = tdu.serialization.ModelCheckpointHandler(
-        gst_pred_config['checkpoints_path'],
+        gst_pred_config['training']['checkpoints_path'],
         device,
         missing_modules_strict=False)
 
     model_comps = gst_utils.create_model_components(
         input_phonemes_shape,
-        (input_gst_size,),
+        gst_embedding_size,
+        gst_weights_size,
         gst_pred_config['model'],
         device)
 
@@ -89,7 +90,7 @@ def _load_gst_pred_components(gst_pred_config,
 
 def _get_acoustic_inference_model(config, device):
 
-    exp_spec, input_phonemes, _, _, _ = torch.load(config['input_sample'],
+    exp_spec, input_phonemes, _, _, _ = torch.load(config['acoustic_input_sample'],
                                                    map_location=device,
                                                    weights_only=True)
 
@@ -99,7 +100,7 @@ def _get_acoustic_inference_model(config, device):
     logging.info('Loading the acoustic model...')
 
     acoustic_comps = _load_acoustic_model_components(
-        acoustic_cfg, config['acoustic_skpt'],
+        acoustic_cfg, config['acoustic_ckpt'],
         (exp_spec.shape[0], exp_spec.shape[1]),
         (input_phonemes.shape[0], input_phonemes.shape[1]),
         device)
@@ -115,7 +116,9 @@ def _get_acoustic_inference_model(config, device):
     logging.info('Composing the inference model...')
 
     return inference.InferenceAcousticModel(acoustic_comps,
-                                            vocoder).to(device)
+                                            vocoder,
+                                            config['deterministic_gst_weight'],
+                                            ).to(device)
 
 
 def _get_gst_predictor_inference_model(config, device):
@@ -126,9 +129,10 @@ def _get_gst_predictor_inference_model(config, device):
     assert config['gst_pred_ckpt'] is not None
     assert config['gst_pred_input_sample'] is not None
 
-    phoneme_repr, _, bert_embeddings, exp_gst = torch.load(config['gst_pred_input_sample'],
-                                                           map_location=device,
-                                                           weights_only=True)
+    phoneme_repr, _, bert_embeddings, exp_gst_emb, exp_gst_w = torch.load(
+        config['gst_pred_input_sample'],
+        map_location=device,
+        weights_only=True)
     bert_embeddings = bert_embeddings.unsqueeze(0).to(device)
 
     with open(config['gst_pred_training_cfg'], 'r', encoding='utf-8') as cfg_f:
@@ -138,17 +142,19 @@ def _get_gst_predictor_inference_model(config, device):
         gst_predictor_cfg,
         config['gst_pred_ckpt'],
         (phoneme_repr.shape[0], phoneme_repr.shape[1]),
-        exp_gst.shape[0],
+        exp_gst_emb.shape[0],
+        exp_gst_w.shape[0],
         device)
 
     gst_pred_comps.eval()
 
     logging.info('Composing the inference model...')
 
+    diff_cfg = gst_predictor_cfg['training']['diffusion']
     diff_handler = diff_utils.DiffusionHandler(
-        diff_utils.LinearScheduler(gst_predictor_cfg['diffusion']['beta_min'],
-                                   gst_predictor_cfg['diffusion']['beta_max'],
-                                   gst_predictor_cfg['diffusion']['n_steps']),
+        diff_utils.LinearScheduler(diff_cfg['beta_min'],
+                                   diff_cfg['beta_max'],
+                                   diff_cfg['n_steps']),
         device
     )
 
@@ -156,13 +162,14 @@ def _get_gst_predictor_inference_model(config, device):
         os.path.join(
             gst_predictor_cfg['data']['dataset_path'],
             'stats',
-            'gst_embedding_stats.pt')
+            'gst_embedding_stats.pt'),
+        map_location=device
     )
 
     return inference.InferenceGSTPredictor(gst_pred_comps,
                                            diff_handler,
                                            scaling_values,
-                                           gst_predictor_cfg['diffusion']['guidance_scale']
+                                           diff_cfg['guidance_scale']
                                            ).to(device)
 
 
@@ -187,13 +194,13 @@ def main(config):
         )
 
         gst_pred_inference_model = _get_gst_predictor_inference_model(config, device)
-        gst_weights = gst_pred_inference_model(  # pylint:disable=not-callable
+        gst_weights, gst_emb = gst_pred_inference_model(  # pylint:disable=not-callable
             phoneme_repr.unsqueeze(0).to(device),
             bert_embeddings.unsqueeze(0).to(device),
             phoneme_mask.unsqueeze(0).to(device),
         )
     else:
-        gst_weights = None
+        gst_weights, gst_emb = None, None
 
     logging.info('Running inference...')
 
@@ -201,11 +208,12 @@ def main(config):
         waveform = acoustic_inference_model(  # pylint:disable=not-callable
             input_phonemes.unsqueeze(0).to(device),
             p_mask.unsqueeze(0).to(device),
-            gst_weights
+            gst_weights,
+            gst_emb
         )
 
     logging.info("Saving the output waveform to '%s'", config['output_path'])
-    torchaudio.save(config['output_path'], waveform.cpu(), 22050)
+    torchaudio.save(config['output_path'], waveform[0].cpu(), 22050)
 
 
 if __name__ == '__main__':
